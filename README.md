69ac0a60df24b9781c4ba4e73a056938cd605597
JackieCisneros <krieger.jacqueline@gmail.com>
2023-03-04 14:54:47 -0700

    creation

---
+++ README.md
@@ -0,0 +1,5 @@
+# phase-2-project
+
+1. First task, build out db with some events to display.  
+
+2. Render all kiddos on the home page.  
---
+++ src/components/HomePageCard.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect} from "react";
+
+
+function HomePageCard(){
+    useEffect(()=>{
+        fetch('')
+    })
+
+    return (
+        <button>
+            <img src=""></img>
+            <img src=""></img>
+        Hello this is my BUTTON!!!
+        </button>
+    )
+}
+
+
+export default HomePageCard;
